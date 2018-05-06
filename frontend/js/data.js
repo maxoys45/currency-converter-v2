2229adb4692e754641a2bf1d6deeb78bc71a4ea4
Max Henchman <maxhenchman@hotmail.co.uk>
2018-05-06 17:05:02 +0100

    initial setup

--- frontend/js/data.js
+++ frontend/js/data.js
@@ -1,9 +1,42 @@
 const data = {
-    main: [
+    "currencies": [
         {
-            key: 'value'
+            "code": "AUD",
+            "sellRate": 1,
+            "name": "Australian Dollars",
+            "sign": "$"
+        },
+        {
+            "code": "USD",
+            "sellRate": .7041,
+            "name": "United States Dollars",
+            "sign": "$"
+        },
+        {
+            "code": "CAD",
+            "sellRate": .9504,
+            "name": "Canadian Dollars",
+            "sign": "$"
+        },
+        {
+            "code": "NZD",
+            "sellRate": .9949,
+            "name": "New Zealand Dollars",
+            "sign": "$"
+        },
+        {
+            "code": "JPY",
+            "sellRate": 78.21,
+            "name": "Japanese Yen",
+            "sign": "¥"
+        },
+        {
+            "code": "GBP",
+            "sellRate": .5472,
+            "name": "British Pounds Sterling",
+            "sign": "£"
         }
     ]
-};
+}
 
 module.exports = data;
--- frontend/js/main.js
+++ frontend/js/main.js
@@ -23,12 +23,17 @@ var tools = {
     },
 
     selectors: {
-
-
+        podCont: document.querySelector('.conversion-pods'),
+        pods: document.querySelectorAll('.conversion_pod'),
+        dropdown: document.querySelectorAll('.conversion_dropdown')
     },
 
     options: {
-
+        activePod: null,
+        activeRate: null,
+        otherPod: null,
+        otherRate: null,
+        otherSelectValue: null
     },
 
     setup: function () {
@@ -36,6 +41,48 @@ var tools = {
     },
 
     events: function () {
+
+        const dropdown = this.selectors.dropdown;
+
+        for (let i = 0; i < dropdown.length; i++) {
+            dropdown[i].addEventListener('change', this.onSelectChange.bind(this), false);
+        }
+
+    },
+
+    onSelectChange: function(e) {
+
+        //this.activePod = e.currentTarget.parentNode.getAttribute("data-id");
+
+        //this.log(this.activePod);
+
+        this.updateConversionRate(e);
+
+    },
+
+    updateConversionRate: function(e) {
+
+        this.activePod = e.currentTarget.parentNode;
+        this.activeRate = this.activePod.querySelector('.conversion_rate');
+        this.otherPod = this.selectors.podCont.querySelector('.conversion_pod:not([data-id="' + this.activePod.dataset.id + '"])');
+        this.otherRate = this.otherPod.querySelector('.conversion_rate');
+        this.otherSelectValue = this.otherPod.querySelector('.conversion_dropdown').value;
+
+        this.log(this.otherSelectValue);
+
+        this.log(this.otherPod, this.otherRate);
+
+        let activeRateString = '1 ' + e.target.value + ' = ' + '1 ' + this.otherSelectValue;
+
+        let otherRateString = '1 ' + this.otherSelectValue + ' = ' + '1 ' + e.target.value;
+
+        this.activeRate.innerHTML = activeRateString;
+        this.otherRate.innerHTML = otherRateString;
+
+
+        //const rate = document.querySelectorAll('.conversion_rate');
+
+
 
     }
 
